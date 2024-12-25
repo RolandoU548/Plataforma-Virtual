cf84d067a80fdb1564efc3eed75921913bfc0c07
Rolando <rolandou548@gmail.com>
2024-12-24 21:53:43 -0400

    Flow Layout Panel

--- src/main/java/com/mycompany/app/AppUI.java
+++ src/main/java/com/mycompany/app/AppUI.java
@@ -1,6 +1,8 @@
 package com.mycompany.app;
 
 import javax.swing.*;
+
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
@@ -21,18 +23,18 @@ public class AppUI {
     }
 
     private static void placeComponents(JPanel panel) {
-        panel.setLayout(null);
+        panel.setLayout(new FlowLayout());
 
         JLabel userLabel = new JLabel("User");
-        userLabel.setBounds(10, 20, 80, 25);
+        userLabel.setSize(80, 25);
         panel.add(userLabel);
 
         JTextField userText = new JTextField(20);
-        userText.setBounds(100, 20, 165, 25);
+        userText.setSize(165, 25);
         panel.add(userText);
 
         JButton loginButton = new JButton("login");
-        loginButton.setBounds(10, 80, 80, 25);
+        loginButton.setSize(80, 25);
         panel.add(loginButton);
 
         loginButton.addActionListener(new ActionListener() {
